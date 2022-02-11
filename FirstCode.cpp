db223889b015b10d911a04a65104b3cb9b267e6a
Shanto <rakibul15-3871@diu.edu.bd>
2022-02-12 01:19:28 +0600

    Welcome to my Cpp world

---
+++ FirstCode.cpp
@@ -0,0 +1,18 @@
+#include<iostream>     
+
+using namespace std;
+
+int main() {
+
+    cout << "Assalamualaikum\n";  // printf 
+
+    cout<<"\t\t  Hello World!\n"; 
+
+    cout<<"\tThis is my first C++ code.\n";
+
+    cout<<"Starting time:\n\t\t\t    8:45 PM\n";
+
+    cout<<"           \t06 December 2021\n";
+
+    return 0;
+}
