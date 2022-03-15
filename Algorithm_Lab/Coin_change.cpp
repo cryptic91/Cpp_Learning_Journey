46040659734efeeb100d8498a5eec2735a579458
Shanto <rakibul15-3871@diu.edu.bd>
2022-03-16 02:04:45 +0600

    Algorithm Lab

--- Algorithm_Lab/Coin_change.cpp
+++ Algorithm_Lab/Coin_change.cpp
@@ -16,21 +16,29 @@ int main () {
     int s, c, num, total = 0;
     cin >> s;
     int p = s;
+
     sort(arr, arr+n, greater<int>() ) ;
-    
+    // for(int i=0; i<n; i++){
+    //     cout << arr[i] << " ";
+    // }
+    // cout << endl;
+
     int i=0;
     
     int A[p];
-    for(int k=0; k<100; k++){
+    for(int k=0; k<p; k++){
         A[p] = 0;
     }
     
     while(s>0){
 
-            if(arr[i] <= s)
-                c = arr[i];
-            i++;
+    if(arr[i] <= s){
+        c = arr[i];
+        
+        // i++;
         num = s/c;
+        cout << "num = " << num 
+        << "c = " << c << endl;
         total += num;
         // for(int j=0; j<num; j++){
             // cout << c << " " ;
@@ -38,23 +46,31 @@ int main () {
         
         A[c] = num;
         
-        s -=num*c;
+        s -= num*c;
+        cout << "S = " << s << endl;
+    }
+
+    i++;
+
     }
     
-    if(s != 0){
+    if(s > 0){
         cout<< "Impossible" << endl;
     }
     else{
         cout << total << endl;
         
-        for(int i=0; i<p; i++){
+        for(i=0; i<p; i++){
+
             if(A[i] != 0){
-                for(int s=0; s<A[i]; s++){
+                int com = A[i];
+                for(int s=1; s<=com; s++){
                     cout << i << " " ;   
                 }
             }
         }
     }
+    // cout << total << endl;
     
     return 0;
 }
---
+++ Algorithm_Lab/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+ // if(s != 0){
+    //     cout<< "Impossible" << endl;
+    // }
+    // else{
+    //     cout << total << endl;
+        
+    //     for(i=0; i<p; i++){
+
+    //         if(A[i] != 0){
+    //             int com = A[i];
+    //             for(int s=1; s<=com; s++){
+    //                 cout << i << " " ;   
+    //             }
+    //         }
+    //     }
+    // }
